bd8e4ef524dbeca2e78431ca3e36e8d74e984ad9
Jesse Rheal <jessecolerheal@live.com>
2023-10-23 19:53:26 -0500

    first commit

---
+++ Main.cpp
@@ -0,0 +1,99 @@
+// g++ Main.cpp -lGL -lGLEW -lGLU -ldl -lglut
+
+#include <GL/glew.h>
+#include <iostream>
+#include <GL/freeglut.h>
+#include "OpenGL445Setup.h"
+#include "PerlinNoise.h"
+#include <cmath>
+#include <memory>
+
+// Globals
+#define CANVAS_WIDTH 720 /* width of canvas - you may need to change this */
+#define CANVAS_HEIGHT 480 /* height of canvas - you may need to change this */
+
+char canvas_Name[] = "Fog";
+double angle = 0.0;
+int texture_index = 0;
+GLuint my_texture_ID;
+
+void display_func() {
+	int sin_texture_index = (int)((cos(texture_index / 30.0f) + 1.0f) / 2.0f * 256.0f);
+	std::cout << sin_texture_index << std::endl;
+
+	// Clears the screen and draws everything
+	glBindTexture(GL_TEXTURE_2D, my_texture_ID);
+	glActiveTexture(GL_TEXTURE0);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLoadIdentity();
+	glTranslated(0.0, 0.0, -10.0);
+	glTranslated(0.0, 0.0, sin_texture_index / 256.0f * 5.0f);
+	
+	float start_x = sin_texture_index % 16;
+	start_x /= 16.0f;
+	float start_y = sin_texture_index / 16;
+	start_y /= 16.0f;
+	float width = 1.0f / 16.0f;
+
+	glBegin(GL_TRIANGLES);
+	glTexCoord2d(start_x,         start_y); 		glVertex3d(-5, -5, 0);
+    glTexCoord2d(start_x + width, start_y); 		glVertex3d(5, -5, 0);
+    glTexCoord2d(start_x + width, start_y + width); glVertex3d(5, 5, 0);
+
+    glTexCoord2d(start_x + width, start_y + width); glVertex3d(5, 5, 0);
+    glTexCoord2d(start_x, 		  start_y + width); glVertex3d(-5, 5, 0);
+    glTexCoord2d(start_x,         start_y); 		glVertex3d(-5, -5, 0);
+	glEnd();
+	glFlush();
+}
+
+void rotate_cube(int ID) {
+	texture_index++;
+	display_func();
+	glutTimerFunc(50, rotate_cube, 1);
+}
+
+int main(int argc, char ** argv) {
+	// Create perlin noise
+	const siv::PerlinNoise perlin{ 123456u };
+	float* noise = new float[256*256*256];
+	for (int yy = 0; yy < 16; yy++) {
+		for (int xx = 0; xx < 16; xx++) {
+			int index_1 = yy * 256 * 4096 + xx * 256;
+
+			for (int y = 0; y < 256; y++) {
+				for (int x = 0; x < 256; x++) {
+					int index_2 = index_1 + (y * 4096 + x);
+					noise[index_2] = perlin.noise3D_01(x * 0.05f, y * 0.05f, (yy * 16 + xx) * 0.05f);
+					//std::cout << i << ", " << j << ", " << k << " | " << noise[(i * 256 + j) * 256 + k] << std::endl;
+				}
+			}
+		}
+	}
+
+	// Initial setup
+	glutInit(&argc, argv);
+	my_setup(CANVAS_WIDTH, CANVAS_HEIGHT, canvas_Name);
+	
+	// Set up the callbacks
+	display_func();
+	glutDisplayFunc(display_func);
+	glutTimerFunc(50, rotate_cube, 1);
+	// glutKeyboardFunc(keyboard_func);
+
+	// Set up textures
+	glEnable(GL_TEXTURE_2D);
+	glGenTextures(1, &my_texture_ID);
+
+	glBindTexture(GL_TEXTURE_2D, my_texture_ID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, 4096, 4096, 0, GL_RED, GL_FLOAT, noise);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	// Start the main loop
+	std::cout << "Any Key Click Will Start" << std::endl;
+	glutMainLoop();
+	delete noise;
+	return 0;
+}
---
+++ OpenGL445Setup.h
@@ -0,0 +1,56 @@
+// setup code for compilation in Visual Studio 2017. Copyright Timothy Newman, 2021-2023.
+// Re-usable, with or without modification, only for CS 445/545 Fall '23 Code Examples, with all re-use documenting the original source in code comments.
+// If modified, nature of modification must be documented. No further use/re-use is authorized without written permission of T. Newman
+
+/***********************************************************
+ OpenGL445Setup.h
+
+   This header file contains initialization function calls and set-ups
+for basic 3D CS 445/545 Open GL (Mesa) programs that use the GLUT/freeglut.
+The initializations involve defining a callback handler (my_reshape_function)
+that sets viewing parameters for orthographic 3D display.
+
+   TSN 2022 and 2023 version - for OpenGL 4.3 w/legacy compatibility
+
+ ************************************************************/
+
+
+/* reshape callback handler - defines viewing parameters (projection) */
+
+void my_3d_projection(int width, int height)
+{
+	GLfloat width_bound, height_bound;
+	width_bound = (GLfloat)width; height_bound = (GLfloat)height;
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(90.0, (double)width / (double)height, 0.0, 100.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+// upper left corner of canvas, in GLUT (not OpenGL!) coordinates
+
+#define STRT_X_POS 25
+#define STRT_Y_POS 25
+
+/* initialization routine */
+
+void my_setup(int width, int height, char *window_name_str)
+{
+	// Allow for current OpenGL4.3 but backwards compatibility to legacy GL 2.1
+	glutInitContextVersion(4, 3);
+	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+	// To get double buffering, uncomment the following line
+	// glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); 
+	// below DisplayMode code line does single buffering - if above line is uncommented,
+	// this single buffering line will have to be commented out
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(width, height);
+	glutInitWindowPosition(STRT_X_POS, STRT_Y_POS);
+
+	glutCreateWindow(window_name_str);   // Note: this ultimately results in GLUT generating a display event
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	glutReshapeFunc(my_3d_projection);
+}
